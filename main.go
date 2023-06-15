09f029c666279fcb3ccaf573144d24b16832387a
rahul <rahulguptaggn765@gmail.com>
2023-06-15 11:36:09 +0200

    Use Printf

--- main.go
+++ main.go
@@ -8,8 +8,15 @@ func main() {
 	const conferenceTickets = 50         // constant - cannot change its value
 	var remainingTickets = 50
 
-	fmt.Println("Welcome to", conferenceName, "booking application")
-	fmt.Println("We have a total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+
+	userName = "rahul"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 
 }
