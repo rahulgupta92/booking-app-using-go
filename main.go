a94bea8748408b45207104ae1bc1592378117db7
rahul <rahulguptaggn765@gmail.com>
2023-06-15 09:05:43 +0200

    Use Println,constants and variables

--- main.go
+++ main.go
@@ -3,6 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to our Conference booking application")
+
+	var conferenceName = "Go conference" // variable - can change its value
+	const conferenceTickets = 50         // constant - cannot change its value
+	var remainingTickets = 50
+
+	fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Println("We have a total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
 	fmt.Println("Get your tickets here to attend")
+
 }
