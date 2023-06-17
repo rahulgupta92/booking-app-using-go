333f75828509378918b7adec45efb562c30021bd
rahul <rahulguptaggn765@gmail.com>
2023-06-17 08:39:39 +0200

    Ask user booking details

--- main.go
+++ main.go
@@ -14,21 +14,23 @@ func main() {
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
 
-	// Pass by value. does not ask for user input. because value is "".
-	fmt.Scan(userName)
+	fmt.Println("Please enter first name")
+	fmt.Scan(&firstName) // Pass by reference. Will ask and read for user input, then store that value in the memory address
 
-	// Pass by reference. Will ask and read for user input, then store that value in the memory address
-	fmt.Scan(&userName)
+	fmt.Println("Please enter last name")
+	fmt.Scan(&lastName) // Pass by reference. Will ask and read for user input, then store that value in the memory address
 
-	// print variable vs its pointer
-	fmt.Println(conferenceName)
-	fmt.Println(&conferenceName)
+	fmt.Println("Please enter email")
+	fmt.Scan(&email) // Pass by reference. Will ask and read for user input, then store that value in the memory address
 
-	//userName = "rahul"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	fmt.Println("Please enter number of tickets")
+	fmt.Scan(&userTickets) // Pass by reference. Will ask and read for user input, then store that value in the memory address
+
+	fmt.Printf("User %v %v booked %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 
 }
